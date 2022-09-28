524b0d3681c9796d1b518151ed662b1b03d0ca3a
Devin Stewart <devin@lucysoft.net>
2022-09-28 11:10:37 -0500

    2022-09-28T16:10:37.698Z - Update status file

--- index.js
+++ index.js
@@ -16,43 +16,83 @@ const init = async (start = false) => {
         host: '0.0.0.0'
     });
 
-    server.route({
-        method: 'POST',
-        path: '/',
-        handler: async (request, h) => {
+    server.route([
+        {
+            method: 'POST',
+            path: '/',
+            handler: async (request, h) => {
 
-            try {
-                const { payload } = request;
-                const validPayload = await Validator.validate(payload);
-                if (validPayload.Type === 'SubscriptionConfirmation' || validPayload.Type === 'UnsubscribeConfirmation') {
-                    // deepcode ignore Ssrf: <validPayload.SubscribeURL has been sanitized by Validator>
-                    Https.get(validPayload.SubscribeURL, (err) => {
+                try {
+                    const { payload } = request;
+                    const validPayload = await Validator.validate(payload);
+                    if (validPayload.Type === 'SubscriptionConfirmation' || validPayload.Type === 'UnsubscribeConfirmation') {
+                        // deepcode ignore Ssrf: <validPayload.SubscribeURL has been sanitized by Validator>
+                        Https.get(validPayload.SubscribeURL, (err) => {
 
-                        if (err) {
-                            console.error(err);
-                            // The route has not been conmfirmed.
-                            return h.response('Error confirming subscription').code(400);
-                        }
+                            if (err) {
+                                console.error(err);
+                                // The route has not been conmfirmed.
+                                return h.response('Error confirming subscription').code(400);
+                            }
 
-                        // The route has been conmfirmed.
+                            // The route has been conmfirmed.
+                            return h.response().code(200);
+                        });
+                    }
+                    else {
+                        const date = new Date().toISOString();
+                        const status = `${date} - ${validPayload.Message}`;
+                        pushGithubMessage('status', status);
                         return h.response().code(200);
-                    });
+                    }
                 }
-                else {
-                    const date = new Date().toISOString();
-                    const status = `${date} - ${validPayload.Message}`;
-                    pushGithubMessage(status);
+                catch (err) {
+                    Fs.writeFileSync('error.txt', err.message);
+                    const TopicArn = await getSnsTopicArn();
+                    await SNS.publish({ TopicArn, Message: 'sns-payload-validator failed' }).promise();
                     return h.response().code(200);
                 }
             }
-            catch (err) {
-                Fs.writeFileSync('error.txt', err.message);
-                const TopicArn = await getSnsTopicArn();
-                await SNS.publish({ TopicArn, Message: 'sns-payload-validator failed' }).promise();
-                return h.response().code(200);
+        },
+        {
+            method: 'POST',
+            path: '/sigV2',
+            handler: async (request, h) => {
+
+                try {
+                    const { payload } = request;
+		    console.log(payload);
+                    const validPayload = await Validator.validate(payload);
+                    if (validPayload.Type === 'SubscriptionConfirmation' || validPayload.Type === 'UnsubscribeConfirmation') {
+                        // deepcode ignore Ssrf: <validPayload.SubscribeURL has been sanitized by Validator>
+                        Https.get(validPayload.SubscribeURL, (err) => {
+
+                            if (err) {
+                                console.error(err);
+                                // The route has not been conmfirmed.
+                                return h.response('Error confirming subscription').code(400);
+                            }
+
+                            // The route has been conmfirmed.
+                            return h.response().code(200);
+                        });
+                    }
+                    else {
+                        const date = new Date().toISOString();
+                        const status = `${date} - ${validPayload.Message}`;
+                        pushGithubMessage('status-sigV2', status);
+                        return h.response().code(200);
+                    }
+                }
+                catch (err) {
+                    Fs.writeFileSync('error-sigV2.txt', err.message);
+                    const TopicArn = await getSnsTopicArn();
+                    await SNS.publish({ TopicArn, Message: 'sns-payload-validator failed' }).promise();
+                    return h.response().code(200);
+                }
             }
         }
-    });
+    ]);
 
     if (start) {
         await server.start();
